36002a26332276a8a4ed850ae495127e517d7f37
BenasB <29711974+BenasB@users.noreply.github.com>
2023-02-08 01:43:31 +0200

    Add EmailReceiver, asserts

--- Microservice.Tests/EmailClientTests.cs
+++ Microservice.Tests/EmailClientTests.cs
@@ -10,15 +10,23 @@ public class EmailClientTests : IClassFixture<MailHogFixture>
 {
     private static readonly ILogger<EmailClient> Logger = new Mock<ILogger<EmailClient>>().Object;
     private readonly IOptions<SmtpOptions> _smtpOptions;
+    private readonly EmailReceiver _emailReceiver;
     private readonly MailHogFixture _fixture;
 
     public EmailClientTests(MailHogFixture fixture)
     {
         _fixture = fixture;
+
         _smtpOptions = Options.Create<SmtpOptions>(new()
         {
-            Host = "localhost",
+            Host = _fixture.Container.Hostname,
             Port = _fixture.Container.GetMappedPublicPort(MailHogFixture.SmtpContainerPort)
+        });
+
+        _emailReceiver = new EmailReceiver(new EmailReceiver.ApiOptions()
+        {
+            Host = _fixture.Container.Hostname,
+            Port = _fixture.Container.GetMappedPublicPort(MailHogFixture.WebContainerPort)
         });
     }
 
@@ -32,10 +40,22 @@ public class EmailClientTests : IClassFixture<MailHogFixture>
         {
             From = "usera@testing.com",
             To = "userb@testing.com",
-            Subject = "Testing is cool!",
+            Subject = "Testing is cool! " + Guid.NewGuid(),
             Body = "This is sent from a test"
         };
 
         await emailClient.SendAsync(request);
+
+        var allEmails = await _emailReceiver.GetAllEmailsAsync();
+
+        // Find the email we just sent by unique subject
+        var email = Assert.Single(allEmails.Where(e => e.Content.Headers.Subject.SingleOrDefault() == request.Subject));
+
+        Assert.Equal(request.From, $"{email.From.Mailbox}@{email.From.Domain}");
+
+        var recipient = Assert.Single(email.To);
+        Assert.Equal(request.To, $"{recipient.Mailbox}@{recipient.Domain}");
+
+        Assert.Equal(request.Body, email.Content.Body);
     }
 }
---
+++ Microservice.Tests/EmailReceiver.cs
@@ -0,0 +1,73 @@
+﻿using MimeKit;
+using System.Net.Http.Json;
+
+namespace Microservice.Tests;
+
+/// <summary>
+/// Queries MailHog's API to retrieve received emails. Primary use is to assert sent emails.
+/// </summary>
+internal class EmailReceiver
+{
+    private readonly HttpClient _client;
+
+	public EmailReceiver(ApiOptions options)
+	{
+        _client = new HttpClient()
+        {
+            BaseAddress = new UriBuilder()
+            {
+                Host = options.Host,
+                Port = options.Port,
+                Path = "/api/v2/"
+            }.Uri,
+        };
+    }
+
+    public async Task<IEnumerable<Email>> GetAllEmailsAsync()
+    {
+        var httpResponse = await _client.GetAsync("messages");
+
+        if (!httpResponse.IsSuccessStatusCode)
+            throw new Exception($"Failed to query MailHog's API. Status code: {httpResponse.StatusCode}");
+
+        var allMessagesResponse = await httpResponse.Content.ReadFromJsonAsync<AllMessagesResponse>();
+
+        return allMessagesResponse?.Items ?? Enumerable.Empty<Email>();
+    }
+
+    public record ApiOptions
+    {
+        public string Host { get; init; } = null!;
+        public int Port { get; init; } = 80;
+    }
+
+    public record Email
+    {
+        public EmailAddress From { get; init; } = new();
+        public IEnumerable<EmailAddress> To { get; init; } = Enumerable.Empty<EmailAddress>();
+        public EmailContent Content { get; set; } = new();
+    }
+
+    public record EmailAddress
+    {
+        public string Mailbox { get; init; } = null!;
+        public string Domain { get; init; } = null!;
+    }
+
+    public record EmailContent
+    {
+        public EmailHeaders Headers { get; set; } = new();
+        public string Body { get; init; } = null!;
+    }
+
+    public record EmailHeaders
+    {
+        public IEnumerable<string> Subject { get; init; } = Enumerable.Empty<string>();
+    }
+
+    private record AllMessagesResponse
+    {
+        public int Total { get; init; }
+        public IEnumerable<Email> Items { get; init; } = Enumerable.Empty<Email>();
+    }
+}
